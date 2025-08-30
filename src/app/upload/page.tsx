376d4672848432dc3cd63a9509547de1a7e3d580
Anggi Fergian <a.fergian.pratama@accenture.com>
2025-08-30 08:07:29 +0700

    feat: trigger new build

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -3,7 +3,7 @@ import { Uploader } from '@/components/common/uploader';
 export default function UploadPage() {
   return (
     <div className="grid gap-4">
-      <h1 className="text-xl font-bold">Image Upload</h1>
+      <h1 className="text-xl font-bold">Upload Image</h1>
       <Uploader />
     </div>
   );
